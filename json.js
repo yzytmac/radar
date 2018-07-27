58be716633adf9b5b103461c71291ed14e6e0e3b
yzytmac <yzytmac@163.com>
2018-07-27 16:50:36 +0800

    init

---
+++ json.js
@@ -0,0 +1,72 @@
+var bdhtos = {
+    "中国新说唱":{
+      "desc":"中国新说唱_百度百科",
+      "img":"https:\/\/ss1.bdstatic.com\/5aAHeD3nKgcUp2HgoI7O1ygwehsv\/media\/ch3\/png\/1111-0.png"
+    },
+    "24楼扔下4磅哑铃":{
+      "desc":"24楼扔下4磅哑铃的孩子,你挽救了整个小区",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308197&di=382ef419446cab70497f0b86233f9361&src=http%3A%2F%2Ft10.baidu.com%2Fit%2Fu%3D1618285440%2C1219831833%26fm%3D55%26app%3D1%26f%3DJPEG%3Fw%3D269%26h%3D240%26s%3DF1B21FD168C12CC81404C9050300E0E0"
+    },
+    "王者荣耀外挂宣判":{
+      "desc":"王者荣耀外挂宣判:作弊者被“封号”后举报",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308197&di=85d5f49ed238847ed694824c987099c8&src=http%3A%2F%2Ft11.baidu.com%2Fit%2Fu%3D2462524672%2C918638669%26fm%3D55%26app%3D1%26f%3DJPEG%3Fw%3D400%26h%3D238%26s%3DF3917081186ECB1DD0B538D7030080A0"
+    },
+    "女子征婚启事火了":{
+      "desc":"女子征婚启事火了,家庭背景令网友吃惊!介绍成功奖励2...",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308198&di=480c78860b006937617237b9e43915df&src=http%3A%2F%2Ft10.baidu.com%2Fit%2Fu%3D3799253751%2C1688010573%26fm%3D78%26app%3D2%26f%3DJPEG%3Fw%3D200%26h%3D200%26s%3D048027F94C62BEC658959089030070D3"
+    },
+    "马航失联事件报告":{
+      "desc":"马航失联事件报告",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308199&di=1bed5de5b623ce7fc9918fb30395643d&src=http%3A%2F%2Ft11.baidu.com%2Fit%2Fu%3D933448708%2C2284148770%26fm%3D55%26app%3D1%26f%3DJPEG%3Fw%3D530%26h%3D281%26s%3D0D22D3177A2673074C8B1EC5030070A8"
+    },
+    "关晓彤爸爸 鹿晗":{
+      "desc":"关晓彤爸爸 鹿晗",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308199&di=c74c060c1a2b05734bf0dfbafbc3bc70&src=http%3A%2F%2Ft11.baidu.com%2Fit%2Fu%3D2825675737%2C1223098832%26fm%3D78%26app%3D2%26f%3DJPEG%3Fw%3D200%26h%3D200%26s%3DE73E15C71CB10BADE618ED7703001032"
+    },
+    "张智霖 海景房":{
+      "desc":"张智霖 海景房",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308200&di=5d97d0ea46641d5914ca96cfaf28624f&src=http%3A%2F%2Ft12.baidu.com%2Fit%2Fu%3D3294387245%2C1529089471%26fm%3D55%26app%3D1%26f%3DPNG%3Fw%3D530%26h%3D350%26s%3D56269740C6BB162E428A1D53030090F8"
+    },
+    "台风路径实时发布系统":{
+      "desc":"台风路径实时发布系统是由浙江省水利信息管理中心主办的台风信息发布系统,系统可及时准确地提供最新最全的台风实时信息、预报路径和历史信息,系统同时整合卫星云图、降雨...",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308200&di=4bf167b99ac6772f3a0614cf90116881&src=http%3A%2F%2Ft10.baidu.com%2Fit%2Fu%3D2215306599%2C2157755142%26fm%3D55%26app%3D1%26f%3DJPEG%3Fw%3D530%26h%3D350%26s%3DBB5123D3B1262B0FB65CFD060100E0C1"
+    },
+    "近百疑似黑奴遗骸":{
+      "desc":"近百疑似黑奴遗骸当时究竟发生了什么?简直太吓人了!",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308200&di=14aa115e878fb3aef393f8f857e3a52c&src=http%3A%2F%2Ft11.baidu.com%2Fit%2Fu%3D2205660946%2C2642851886%26fm%3D55%26app%3D1%26f%3DJPEG%3Fw%3D530%26h%3D350%26s%3D2E124F81CCDA1ACE2C2D3CB10300C090"
+    },
+    "子弹打进幼儿园":{
+      "desc":"子弹打进幼儿园,这瞬间就被惊到了,毕竟,幼儿园有好多幼儿、也有老师等,这就是说他们的生命安全受到了威胁!但值得庆幸的是,尽管",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308201&di=ed1a98da3d93552b82498e6234e1a06f&src=http%3A%2F%2Ft11.baidu.com%2Fit%2Fu%3D1377526561%2C1448931966%26fm%3D55%26app%3D1%26f%3DJPEG%3Fw%3D520%26h%3D350%26s%3DC30041ACCA013ACC2EB5B99603007080"
+    },
+    "印度拟让苹果退网":{
+      "desc":"印度拟让苹果退网:退网最后期限2019年1月19日截止",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308202&di=2750bf6fb75b10b5f05f8fb1ef53c5ec&src=http%3A%2F%2Ft11.baidu.com%2Fit%2Fu%3D2584121485%2C3961566400%26fm%3D78%26app%3D2%26f%3DJPEG%3Fw%3D200%26h%3D200%26s%3DB960408546220113448C7DB00300E010"
+    },
+    "刘銮雄咒骂少女":{
+      "desc":"刘銮雄咒骂少女:积你个福!网友:缺少风度",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308202&di=ba31f8d43d84e3d4197096b9f0235d7d&src=http%3A%2F%2Ft10.baidu.com%2Fit%2Fu%3D2752248636%2C2144176849%26fm%3D55%26app%3D1%26f%3DPNG%3Fw%3D530%26h%3D350%26s%3D3788BC45C6404D574B90B4A303007013"
+    },
+    "银护3开除詹姆斯":{
+      "desc":"银护3开除詹姆斯是什么情况?",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308203&di=f9f970916e8b98debeb1259cac2a2e4e&src=http%3A%2F%2Ft12.baidu.com%2Fit%2Fu%3D3990738413%2C4101643629%26fm%3D55%26app%3D1%26f%3DJPEG%3Fw%3D530%26h%3D350%26s%3DEEB4C54B564A1157D89A68BF03008012"
+    },
+    "英模回应王菊解约":{
+      "desc":"英模回应王菊解约:8年合约违约金6位数希望和解",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308204&di=9f9d95a999382918584bdead74323d55&src=http%3A%2F%2Ft11.baidu.com%2Fit%2Fu%3D3503134231%2C3485389773%26fm%3D55%26app%3D1%26f%3DJPEG%3Fw%3D530%26h%3D350%26s%3DB624D44FCE0103553233B0A90300C018"
+    },
+    "梅西偶遇纳达尔":{
+      "desc":"梅西偶遇纳达尔进行了“简单但友好”的对话",
+      "img":"https:\/\/ss0.bdstatic.com\/9bA1vGfa2gU2pMbfm9GUKT-w\/timg?wisealaddin&size=f530_350&quality=100&sec=1532308204&di=1d0d2306b8fd4c539e8610261c3c7ea8&src=http%3A%2F%2Ft11.baidu.com%2Fit%2Fu%3D1412762140%2C107494580%26fm%3D55%26app%3D1%26f%3DJPG%3Fw%3D530%26h%3D350%26s%3D27345587236202AC00D18DBD0300A003"
+    }
+  }
+for(var i in bdhtos){
+	$(".wrap").append($('<div class="list"><a href="https://yz.m.sm.cn/s?from=wm848382&q='+i+'">'+
+     '<div class="imgbox"><img src="'+bdhtos[i].img+'"></div>'+
+     '<div class="txtbox">'+
+	        '<h2>'+i+'</h2>'+
+	       '<p>'+bdhtos[i].desc+'</p>'+
+          '<time></time>'+
+     '</div>'+
+'</div>'));
+}
